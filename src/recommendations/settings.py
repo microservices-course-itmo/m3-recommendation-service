be7aaf6f6cc662e39bf943d4e07a81f033899b93
Alexander Komshanov <a.komshanovjr@gmail.com>
2021-03-04 14:25:55 +0300

    add static

--- src/recommendations/settings.py
+++ src/recommendations/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
 SWAGGER_SETTINGS = {
    'USE_SESSION_AUTH': False
