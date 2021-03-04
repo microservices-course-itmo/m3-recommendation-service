953d9d0e9ca25e26c22f7d2a6e25d609e92d339f
Alexander Komshanov <a.komshanovjr@gmail.com>
2021-03-04 15:32:28 +0300

    fix static url (again)

--- src/recommendations/settings.py
+++ src/recommendations/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/ml3-recommendation-service/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 SWAGGER_SETTINGS = {
