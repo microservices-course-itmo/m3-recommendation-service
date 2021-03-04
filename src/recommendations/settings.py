8694e193175d1e1c1870342b96df78c75bc6d3f6
Alexander Komshanov <a.komshanovjr@gmail.com>
2021-03-04 14:55:01 +0300

    change static url

--- src/recommendations/settings.py
+++ src/recommendations/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_URL = 'ml3-recommendation-service/static/'
+# STATIC_ROOT = '/static/'
 
 SWAGGER_SETTINGS = {
    'USE_SESSION_AUTH': False
